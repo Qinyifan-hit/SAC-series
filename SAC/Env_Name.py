81c7100ef1bc4af00c9bd4807c4b1f7710b0ac20
Qin Yifan <156170866+Qinyifan-hit@users.noreply.github.com>
2025-03-29 17:25:25 +0800

    Create Env_Name.py

---
+++ SAC/Env_Name.py
@@ -0,0 +1,20 @@
+Name = {
+    1: "Pendulum-v1",
+    2: "LunarLanderContinuous-v2",
+    3: "Humanoid-v4",
+    4: "HalfCheetah-v4",
+    5: "BipedalWalker-v3",
+    6: "BipedalWalkerHardcore-v3",
+    7: "MountainCarContinuous-v0",
+    8: "Ant-v2"
+}
+Brief_Name = {
+    1: "Pv1",
+    2: "LLCv2",
+    3: "Hv4",
+    4: "HCv4",
+    5: "BWv3",
+    6: "BWHv3",
+    7: "MCCv0",
+    8: "Av2"
+}
